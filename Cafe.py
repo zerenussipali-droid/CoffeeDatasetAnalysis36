05faaa36e0ebb0750a6aa99f04e921c15a9c307d
Asus <zere.nussipali@narxoz.kz>
2026-05-19 11:46:55 +0500

    first commit 1-3

---
+++ Cafe.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+class Cafe:
+    def __init__(self, cafe, city, avg_check, rating):
+        self.cafe = cafe
+        self.city = city
+        self.avg_check = avg_check
+        self.rating = rating
+
+    def to_dict(self):
+        return {
+            "cafe": self.cafe,
+            "city": self.city,
+            "avg_check": self.avg_check,
+            "rating": self.rating
+        }
+
+cafes = [
+    Cafe("Coffee Boom", "Almaty", 4500, 4.8),
+    Cafe("Urban Coffee", "Astana", 5200, 4.6),
+    Cafe("Qazaq Coffee", "Shymkent", 3900, 4.7),
+    Cafe("Cappuccino", "Almaty", 6100, 4.9),
+    Cafe("Coffee Point", "Karaganda", 3400, 4.5)
+]
+
+data = [cafe.to_dict() for cafe in cafes]
+df = pd.DataFrame(data)
+df.to_csv("cafes.csv", index=False)
+
+print("cafes.csv file created successfully!")
